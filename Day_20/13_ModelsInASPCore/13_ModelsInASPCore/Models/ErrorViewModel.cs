12293be6eebd45930706c60bf3e4ff2cd9efed17
shivabishwokarma28 <shivabishwokarma90@gmail.com>
2026-03-09 14:20:02 +0545

    Day 21 Task

--- Day_20/13_ModelsInASPCore/13_ModelsInASPCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _13_ModelsInASPCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Day_20/15_ToDoApp/15_ToDoApp/Models/ToDoApp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _15_ToDoApp.Models
-{
-    public class ToDoApp
-    {
-        public int Id { get; set; }
-        public string Task {  get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
