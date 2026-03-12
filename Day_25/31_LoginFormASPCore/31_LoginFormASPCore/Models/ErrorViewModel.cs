1003be88d5f7260b245be41b9f4e38aa875fe222
shivabishwokarma28 <shivabishwokarma90@gmail.com>
2026-03-12 12:03:31 +0545

    Day 26 Task

--- Day_25/31_LoginFormASPCore/31_LoginFormASPCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _31_LoginFormASPCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
