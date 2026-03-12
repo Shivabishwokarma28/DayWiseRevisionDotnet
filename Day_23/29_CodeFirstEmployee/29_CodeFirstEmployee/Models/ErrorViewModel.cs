60cf964b3114e07ee9d28f3e824ec970d3ceed07
shivabishwokarma28 <shivabishwokarma90@gmail.com>
2026-03-12 10:47:45 +0545

    Day 24 Task

--- Day_23/29_CodeFirstEmployee/29_CodeFirstEmployee/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _29_CodeFirstEmployee.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
